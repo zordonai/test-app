2a31a5b71aa0168999987f8169c66d6781bfae04
Victor Bicudo <victor_bicudo@outlook.com>
2023-12-12 00:25:16 -0300

    update

--- example.js
+++ example.js
@@ -1,3 +1,7 @@
 console.log("example 1");
 console.log("example 1");
-console.log("example 1");
+console.log("example 2");
+console.log("example 3");
+console.log("example 4");
+console.log("example 5");
+console.log("example 6");
