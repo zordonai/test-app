54dba43b49cda32d699e3451c27244a3e79eb57c
Victor Bicudo <victor_bicudo@outlook.com>
2023-12-13 18:56:28 -0300

    update

--- example-4.js
+++ example-4.js
@@ -3,3 +3,11 @@ console.log("example-4");
 if (true) {
   console.log("veio aqui");
 }
+
+if (true) {
+  console.log("veio aqui não");
+}
+
+const print = (text) => console.log(text);
+
+const doSomething = (a, b) => a + b;
