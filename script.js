48277c0735d8bb62ecc12afb49b93fd183fe9015
Victor Bicudo <victor_bicudo@outlook.com>
2023-12-14 12:57:32 -0300

    update

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
 console.log("vim aqui 12");
-
-if (true) {
-  console.log("veio aqui");
-}
-
-if (true) {
-  console.log("veio aqui não");
-}
-
-const print = (text) => console.log(text);
-
-const doSomething = (a, b) => a + b;
